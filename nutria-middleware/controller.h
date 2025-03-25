87c794b46a1a578969a70ee3d861643c5b183cea
GeonhoRoh <roh517@naver.com>
2025-03-25 13:57:14 -0500

    FEAT: create dll func entry

--- nutria-middleware/controller.h
+++ nutria-middleware/controller.h
@@ -6,18 +6,18 @@
 #include <SharedMemory.h>
 
 // Define export/import macro for DLL usage
-#ifdef BUILDING_DLL
-#define DLL_API __declspec(dllexport)  // Export when building the DLL
-#else
-#define DLL_API __declspec(dllimport)  // Import when using the DLL
-#endif
+//#ifdef BUILDING_DLL
+//#define DLL_API __declspec(dllexport)  // Export when building the DLL
+//#else
+//#define DLL_API __declspec(dllimport)  // Import when using the DLL
+//#endif
 
 struct Message {
     int cmd;
     int option;
 };
 
-class DLL_API Controller {
+class Controller {
 public:
     // Constructor
     Controller();
@@ -37,5 +37,9 @@ private:
     void initialize(); // Method to initialize the controller
     void cleanup();    // Method to clean up resources
 };
+
+//// These functions are used to create/destroy Controller instances
+//extern "C" DLL_API Controller* createController(); // To create a Controller instance
+//extern "C" DLL_API void destroyController(Controller* controller); // To destroy a Controller instance
 
 #endif // CONTROLLER_H
--- nutria-middleware/dllmain.cpp
+++ nutria-middleware/dllmain.cpp
@@ -1,19 +1,38 @@
-//// dllmain.cpp : Defines the entry point for the DLL application.
-//#include "pch.h"
-//
-//BOOL APIENTRY DllMain( HMODULE hModule,
-//                       DWORD  ul_reason_for_call,
-//                       LPVOID lpReserved
-//                     )
-//{
-//    switch (ul_reason_for_call)
-//    {
-//    case DLL_PROCESS_ATTACH:
-//    case DLL_THREAD_ATTACH:
-//    case DLL_THREAD_DETACH:
-//    case DLL_PROCESS_DETACH:
-//        break;
-//    }
-//    return TRUE;
-//}
-//
+// dllmain.cpp : Defines the entry point for the DLL application.
+#include "pch.h"
+#include <iostream>
+#include "controller.h"
+
+extern "C" __declspec(dllexport) bool sendMessage(int cmd, int option);
+
+
+Controller* controller;
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        std::cout << "MIDDLEWARE DLL ATTACHED\n"; // Fixed std::cout statement
+        controller = new Controller();
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        if (controller) {
+            delete controller; // Clean up the controller object when the DLL is unloaded
+        }
+        break;
+    }
+    return TRUE;
+}
+
+bool sendMessage(int cmd, int option) {
+    if (controller) {
+        return controller->sendMSG(cmd, option);
+    }
+    return false; // Return false if the controller is not initialized
+}
+
