c6c214dd0aec6e786922d3bdeb1cf6701f86369c
GeonhoRoh <roh517@naver.com>
2025-03-25 17:49:42 -0500

    FEAT: add controller init to dll

--- nutria-middleware/dllmain.cpp
+++ nutria-middleware/dllmain.cpp
@@ -3,10 +3,8 @@
 #include <iostream>
 #include "controller.h"
 
-extern "C" __declspec(dllexport) bool sendMessage(int cmd, int option);
-
-
-Controller* controller;
+extern "C" __declspec(dllexport) bool sendMessage(Controller* controller, int cmd, int option);
+extern "C" __declspec(dllexport) Controller* initController();
 
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
@@ -17,22 +15,21 @@ BOOL APIENTRY DllMain( HMODULE hModule,
     {
     case DLL_PROCESS_ATTACH:
         std::cout << "MIDDLEWARE DLL ATTACHED\n"; // Fixed std::cout statement
-        controller = new Controller();
     case DLL_THREAD_ATTACH:
     case DLL_THREAD_DETACH:
     case DLL_PROCESS_DETACH:
-        if (controller) {
-            delete controller; // Clean up the controller object when the DLL is unloaded
-        }
         break;
     }
     return TRUE;
 }
 
-bool sendMessage(int cmd, int option) {
+bool sendMessage(Controller* controller, int cmd, int option) {
     if (controller) {
         return controller->sendMSG(cmd, option);
     }
     return false; // Return false if the controller is not initialized
 }
 
+Controller* initController() {
+    return new Controller();
+}
